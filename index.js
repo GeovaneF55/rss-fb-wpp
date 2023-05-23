9333e5dd07eb4c0b5d9cfd3f58ad66be2b7b65d5
GeovaneF55 <geovanefss@gmail.com>
2023-05-24 00:58:50 +0100

    IFacebook RSS first test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let parser = new Parser();
 
 (async () => {
 
-    let feed = await parser.parseURL('https://daily-dev-tips.com/sitemap.xml');
+    let feed = await parser.parseURL('http://fetchrss.com/rss/646d514eebaa4b5384513e63646d515d7575985e0d089b32.xml'); // TODO: Change with https://www.devextent.com/xml-rss-feed-nodejs/
 
     console.log(feed.title);
 
